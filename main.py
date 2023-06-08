8598e60cb064f92cf03de04a01147815b9d47c75
CambridgeZ <1376734195@qq.com>
2023-06-08 17:20:10 +0800

    删除掉对于我而言多余的功能，删除掉调用api的相关

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import lib.img_function as predict
 import lib.img_math as img_math
 import lib.img_excel as img_excel
 import lib.img_sql as img_sql
-from lib.img_api import api_pic
+# from lib.img_api import api_pic
 import lib.screencut as screencut
 from threading import Thread
 from tkinter import ttk
@@ -69,14 +69,14 @@ class Surface(ttk.Frame):
         top.pack(side=TOP, expand=1, fill=tk.Y)
         reset_ctl = ttk.Button(top, text="重置窗口", width=10, command=self.reset)
         reset_ctl.pack(side=LEFT)
-        L1 = ttk.Label(top, text='网络地址:')
-        L1.pack(side=LEFT)
-        self.p1 = StringVar()
-        self.user_text = ttk.Entry(top, textvariable=self.p1, width=45)
-        self.user_text.pack(side = LEFT)
-        self.user_text.bind('<Key-Return>', self.url_pic2)
-        url_ctl = ttk.Button(top, text="识别网络图片", width=20, command=self.url_pic)
-        url_ctl.pack(side=RIGHT)
+        # L1 = ttk.Label(top, text='网络地址:')
+        # L1.pack(side=LEFT)
+        self.p1 = StringVar() 
+        # self.user_text = ttk.Entry(top, textvariable=self.p1, width=45)
+        # self.user_text.pack(side = LEFT)
+        # self.user_text.bind('<Key-Return>', self.url_pic2)
+        # url_ctl = ttk.Button(top, text="识别网络图片", width=20, command=self.url_pic)
+        # url_ctl.pack(side=RIGHT)
 
         self.pack(fill=tk.BOTH, expand=tk.YES, padx="10", pady="10")
         frame_left.pack(side=LEFT, expand=1)
@@ -87,16 +87,16 @@ class Surface(ttk.Frame):
         self.image_ctl = ttk.Label(frame_left)
         self.image_ctl.pack(anchor="nw")
 
-        ttk.Label(frame_right1, text='形状定位车牌位置：').grid(column=0, row=0, sticky=tk.W)
+        # ttk.Label(frame_right1, text='形状定位车牌位置：').grid(column=0, row=0, sticky=tk.W)
         from_pic_ctl = ttk.Button(frame_right2, text="来自图片", width=20, command=self.from_pic)
         from_pic_ctl2 = ttk.Button(frame_right2, text="路径批量识别", width=20, command=self.from_pic2)
-        from_vedio_ctl = ttk.Button(frame_right2, text="打开/关闭摄像头", width=20, command=self.from_vedio)
-        from_video_ctl = ttk.Button(frame_right2, text="拍照并识别", width=20, command=self.video_pic)
+        # from_vedio_ctl = ttk.Button(frame_right2, text="打开/关闭摄像头", width=20, command=self.from_vedio)
+        # from_video_ctl = ttk.Button(frame_right2, text="拍照并识别", width=20, command=self.video_pic)
         from_img_pre = ttk.Button(frame_right2, text="查看预处理图像", width=20, command=self.show_img_pre)
         clean_ctrl = ttk.Button(frame_right2, text="清除识别数据", width=20, command=self.clean)
-        exit_ctrl = ttk.Button(frame_right2, text="api再次识别", width=20, command=self.api_ctl)
-        self.cut_ctrl = ttk.Button(frame_right2, text="截图识别", width=20, command=self.cut_pic)
-        camera_ctrl = ttk.Button(frame_right2, text="开关摄像头实时识别(测试)", width=20, command=self.camera_flag)
+        # exit_ctrl = ttk.Button(frame_right2, text="api再次识别", width=20, command=self.api_ctl)
+        # self.cut_ctrl = ttk.Button(frame_right2, text="截图识别", width=20, command=self.cut_pic)
+        # camera_ctrl = ttk.Button(frame_right2, text="开关摄像头实时识别(测试)", width=20, command=self.camera_flag)
 
         self.roi_ctl = ttk.Label(frame_right1)
         self.roi_ctl.grid(column=0, row=1, sticky=tk.W)
@@ -105,15 +105,15 @@ class Surface(ttk.Frame):
         self.r_ctl.grid(column=0, row=3, sticky=tk.W)
         self.color_ctl = ttk.Label(frame_right1, text="", width="20")
         self.color_ctl.grid(column=0, row=4, sticky=tk.W)
-        self.cut_ctrl.pack(anchor="se", pady="5")
-        camera_ctrl.pack(anchor="se", pady="5")
-        from_vedio_ctl.pack(anchor="se", pady="5")
-        from_video_ctl.pack(anchor="se", pady="5")
+        # self.cut_ctrl.pack(anchor="se", pady="5")
+        # camera_ctrl.pack(anchor="se", pady="5")
+        # from_vedio_ctl.pack(anchor="se", pady="5")
+        # from_video_ctl.pack(anchor="se", pady="5")
         from_pic_ctl2.pack(anchor="se", pady="5")
         from_pic_ctl.pack(anchor="se", pady="5")
         from_img_pre.pack(anchor="se", pady="5")
         clean_ctrl.pack(anchor="se", pady="5")
-        exit_ctrl.pack(anchor="se", pady="5")
+        # exit_ctrl.pack(anchor="se", pady="5")
 
         ttk.Label(frame_right1, text='-------------------------------').grid(column=0, row=5, sticky=tk.W)
         ttk.Label(frame_right1, text='颜色定位车牌位置：').grid(column=0, row=6, sticky=tk.W)
@@ -133,27 +133,6 @@ class Surface(ttk.Frame):
 
         self.predictor = predict.CardPredictor()
         self.predictor.train_svm()
-
-    def cut_pic(self):
-        #最小化主窗口
-        win.state('icon')
-        sleep(0.2)
-        filename = "tmp/cut.gif"
-        im =ImageGrab.grab()
-        im.save(filename)
-        im.close()
-        #显示全屏幕截图
-        w = screencut.MyCapture(win, filename)
-        self.cut_ctrl.wait_window(w.top)
-
-        #截图结束，恢复主窗口，并删除临时的全屏幕截图文件
-        win.state('normal')
-        os.remove(filename)
-        self.cameraflag = 0
-        self.pic_path = "tmp/cut.png"
-        self.clean()
-        self.pic_source = "来自截图"
-        self.pic(self.pic_path)
 
     def reset(self):
         self.reset2()
@@ -255,51 +234,6 @@ class Surface(ttk.Frame):
             self.r_ct2.configure(text="")
             self.color_ct2.configure(state='disabled')
 
-    def camera_flag(self):
-        if not self.thread_run:
-            tkinter.messagebox.showinfo('提示', '请点击    [打开摄像头]    按钮！')
-            return
-        if not self.cameraflag:
-            self.cameraflag = 1
-            self.thread2 = threading.Thread(target=self.video_pic2)
-            self.thread2.setDaemon(True)
-            self.thread2.start()
-            self.thread_run2 = True
-        else:
-            self.cameraflag = 0
-            self.thread_run2 = False
-            print("关闭摄像头实时识别 self.cameraflag", self.cameraflag)
-
-    def from_vedio(self):
-        if self.thread_run:
-            if self.camera.isOpened():
-                self.camera.release()
-                print("关闭摄像头")
-                self.camera = None
-                self.thread_run = False
-            return
-        if self.camera is None:
-            self.camera = cv2.VideoCapture(1)
-            if not self.camera.isOpened():
-                self.camera = None
-                print("没有外置摄像头")
-                self.camera = cv2.VideoCapture(0)
-                if not self.camera.isOpened():
-                    print("没有内置摄像头")
-                    tkinter.messagebox.showinfo('警告', '摄像头打开失败！')
-                    self.camera = None
-                    return
-                else:
-                    print("打开内置摄像头")
-            else:
-                print("打开外置摄像头")
-        self.pic_source = "摄像头"
-        self.cameraflag = 0
-        self.thread = threading.Thread(target=self.vedio_thread)
-        self.thread.setDaemon(True)
-        self.thread.start()
-        self.thread_run = True
-
     def pic(self, pic_path):
         self.apistr = None
         img_bgr = img_math.img_read(pic_path)
@@ -393,97 +327,9 @@ class Surface(ttk.Frame):
                 print("批量识别结束")
                 return
 
-    def vedio_thread(self):
-        self.thread_run = True
-        while self.thread_run:
-            _, self.img_bgr = self.camera.read()
-            self.imgtk = self.get_imgtk(self.img_bgr)
-            self.image_ctl.configure(image=self.imgtk)
-        print("run end")
-
-    def video_pic2(self):
-        self.thread_run2 = True
-        predict_time = time.time()
-        while self.thread_run2:
-            if self.cameraflag:
-                if time.time() - predict_time > 2:
-                    print("实时识别中self.cameraflag", self.cameraflag)
-                    cv2.imwrite("tmp/test.jpg", self.img_bgr)
-                    self.pic_path = "tmp/test.jpg"
-                    self.pic(self.pic_path)
-                    predict_time = time.time()
-        print("run end")
-
-    def video_pic(self):
-        if not self.thread_run:
-            tkinter.messagebox.showinfo('提示', '请点击    [打开摄像头]    按钮！')
-            return
-        self.thread_run = False
-        self.thread_run2 = False
-        _, img_bgr = self.camera.read()
-        cv2.imwrite("tmp/test.jpg", img_bgr)
-        self.pic_path = "tmp/test.jpg"
-        self.clean()
-        self.pic(self.pic_path)
-        print("video_pic")
-
-    def url_pic(self):
-        IMAGE_URL = self.getuser()
-        URL_len = len(IMAGE_URL)
-        if (IMAGE_URL == ""):
-            tkinter.messagebox.showinfo('提示', '请输入网址！')
-            return
-        if (URL_len > 150):
-            tkinter.messagebox.showinfo('提示', '网址过长！')
-            return
-        r = requests.get(IMAGE_URL)
-        with open("tmp/url.png", 'wb') as f:
-            f.write(r.content)
-        # print(IMAGE_URL)
-        self.thread_run = False
-        self.thread_run2 = False
-        self.cameraflag=0
-        self.pic_path = "tmp/url.png"
-        self.clean()
-        self.pic_source = "网络地址：" + IMAGE_URL
-        self.pic(self.pic_path)
-
-    def url_pic2(self, self2):
-        self.url_pic()
-
     def getuser(self):
         user = self.user_text.get()
         return user
-
-    def api_ctl(self):
-        if self.thread_run:
-            return
-        self.thread_run = False
-        self.thread_run2 = False
-        colorstr, textstr = api_pic(self.pic_path)
-        self.apistr = colorstr + textstr
-        self.show_roi1(textstr, None, colorstr)
-        self.show_roi2(textstr, None, colorstr)
-        localtime = time.asctime(time.localtime(time.time()))
-        value = [localtime, None, None, None, None, self.apistr, self.pic_source]
-        print(localtime, "|", "|", "| ", self.apistr, "|", self.pic_source)
-        img_excel.excel_add(value)
-        img_sql.sql(value[0], value[1], value[2], value[3], value[4], value[5], value[6])
-
-    def api_ctl2(self, pic_path66):
-        if self.thread_run:
-            return
-        self.thread_run = False
-        self.thread_run2 = False
-        colorstr, textstr = api_pic(pic_path66)
-        self.apistr = colorstr + textstr
-        self.show_roi1(textstr, None, colorstr)
-        self.show_roi2(textstr, None, colorstr)
-        localtime = time.asctime(time.localtime(time.time()))
-        value = [localtime, None, None, None, None, self.apistr, self.pic_source]
-        print(localtime, "|", "|", "| ", self.apistr, "|", self.pic_source)
-        img_excel.excel_add(value)
-        img_sql.sql(value[0], value[1], value[2], value[3], value[4], value[5], value[6])
 
     def show_img_pre(self):
         if self.thread_run:
